835199c2942d065d34f81a1182948c46e7946c87
Henrique Vieira <hbaggiovieira@gmail.com>
2022-05-04 17:49:33 -0300

    First commit

---
+++ app/src/main/java/com/nttdata/educational/app/welcome/ui/WelcomeActivity.kt
@@ -0,0 +1,12 @@
+package com.nttdata.educational.app.welcome.ui
+
+import android.app.Activity
+import android.os.Bundle
+import com.nttdata.educational.R
+
+class WelcomeActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_welcome)
+    }
+}
